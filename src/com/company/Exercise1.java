d0bd502554aed7171f269f3861c19add7a4d495d
Atfan <atfan235@gmail.com>
2024-04-21 21:32:27 +0300

    Exercise1

---
+++ src/com/company/Exercise1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Print();
+    }
+    private static void Print(){
+        System.out.print("\"Your time is limited,\n\tso don\'t waste it\n" +
+                "\t\tliving someone else\'s life\"\n\t\t\tSteve Jobs");
+    }
+}
