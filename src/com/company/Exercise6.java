753816989309bd48ec2eeb6a9608e04755c4d00e
Atfan <atfan235@gmail.com>
2024-04-27 13:03:20 +0300

    Exercise6

---
+++ src/com/company/Exercise6.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Exercise6 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        for(;;) {
+            System.out.println("Введіть кількість метрів: ");
+            double metr = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("Введіть бажану метрику(exit для виходу): ");
+            String choise = scanner.nextLine().toLowerCase();
+
+            if(choise.equals("exit")) {
+                return;
+            }else{
+                switch (choise) {
+                    case "mil":
+                        System.out.println(MInMil(metr)+" "+choise+" в "+metr+" m");
+                        break;
+                    case "inc":
+                        System.out.println(MInInc(metr)+" "+choise+" в "+metr+" m");
+                        break;
+                    case "yd":
+                        System.out.println(MInYd(metr)+" "+choise+" в "+metr+" m");
+                        break;
+                    default:
+                        System.out.println("Невідомий одиниця виміру.");
+                }
+            }
+
+        }
+
+    }
+
+    private static double MInMil(double m){
+        return m*0.00062137;
+    }
+
+    private static double MInInc(double m){
+        return m*39.3701;
+    }
+
+    private static double MInYd(double m){
+        return m*1.0936;
+    }
+
+}
