c93f4b50d5a9d145d20b57b6007737db091e2518
Atfan <atfan235@gmail.com>
2024-04-27 14:22:59 +0300

    exercise9(upd)

--- src/com/homework01/Exercise9.java
+++ src/com/homework01/Exercise9.java
@@ -18,7 +18,7 @@ public class Exercise9 {
         int counterPositiv=0;
         int counterZero=0;
 
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             if (arr[i]>max) {
                 max = arr[i];
             }
