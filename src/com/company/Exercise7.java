c0fcf26c955672305ce256d26013820d82cdcf54
Atfan <atfan235@gmail.com>
2024-04-27 13:25:11 +0300

    Exercise7

---
+++ src/com/company/Exercise7.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        int[] arr=new int[2];
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введіть перше число: ");
+        arr[0] = input.nextInt();
+        System.out.println("Введіть друге число: ");
+        arr[1] = input.nextInt();
+
+        if(arr[0]>arr[1]){
+            swap(arr);
+        }
+
+        Print(arr);
+    }
+    private static void Print(int[] arr){
+        System.out.print("Не парні числа з діапазону від "+arr[0]+" до "+arr[1] +": ");
+        for(int i = arr[0]; i <= arr[1]; i++){
+            if(i % 2 == 1) {
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println();
+    }
+    private static void swap(int[] arr){
+        int temp = arr[0];
+        arr[0] = arr[1];
+        arr[1] = temp;
+    }
+}
